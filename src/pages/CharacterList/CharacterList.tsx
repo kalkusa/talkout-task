a098a1ec3d307191a0ab0e9168280f825e80450f
Arkadiusz Kałkus <kalkus@10g.pl>
2021-01-03 03:54:38 +0100

    Clean up

--- src/pages/CharacterList/CharacterList.tsx
+++ src/pages/CharacterList/CharacterList.tsx
@@ -11,44 +11,17 @@ export const CharacterList: React.FC<Props> = (props) => {
   const [hasMore, setHasMore] = useState(false);
   const [page, loaderRef, scrollerRef] = useInfiniteScroll({ hasMore });
 
-  // useEffect(() => {
-  //   const apiReader = new MarvelApiReader();
-  //   async function fetchCharacters() {
-  //     const characters = await apiReader.getCharacters(50, 0);
-  //     setCharacterList(characters);
-  //   }
-  //   fetchCharacters();
-  // }, []);
-
-  console.log("page: %o", page);
-
   useEffect(() => {
     (async () => {
-      console.log("effect");
       const apiReader = new MarvelApiReader();
       const realPage = page + 1;
-      // console.log("realPage: %o", realPage);
       const data = await apiReader.getCharacters(20, (realPage - 1) * 20);
-      setHasMore(true);
-      // console.log("data:%o", data);
+      setHasMore(true); //api returns so huge amount of characters we can ignore it for now
       setItems((prev) => [...prev, ...data]);
     })();
   }, [page]);
 
-  // if (items.length === 0) {
-  //   return <div>Loading...</div>;
-  // }
-
-  console.log("items: %o", items.length);
-
   return (
-    // <div className="character-list-container" ref={scrollerRef}>
-    //   {items.map((character) => (
-    //     <CharacterBox {...character} key={character.id} />
-    //   ))}
-    //   {/* {hasMore && <div ref={loaderRef}>Loading…</div>} */}
-    //   <div ref={loaderRef}>Loading…</div>
-    // </div>
     <div ref={scrollerRef} className="character-list-container">
       {items.map((character) => (
         <CharacterBox {...character} key={character.id} />
